dec9fd7c0cea25cba63f13fdaacb87344161ecf0
someHui <ratalk1@163.com>
2017-01-25 18:13:02 +0800

    Launcher?Joke

--- app/src/main/java/cn/somehui/fightthelife/LifeActivity.java
+++ app/src/main/java/cn/somehui/fightthelife/LifeActivity.java
@@ -7,15 +7,24 @@ import android.content.Intent;
 
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.ResolveInfo;
 import android.graphics.Rect;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cn.somehui.baselibrary.clicklistener.OnIntentClickListener;
 import cn.somehui.baselibrary.permission.SelfPermissionCallback;
@@ -26,6 +35,8 @@ public class LifeActivity extends MyBaseActivity {
     private ViewGroup mBgViewGroup;
     private TransferClock mClockView;
     private View mReaderBtn;
+    private View mCenterBtn;
+    private RecyclerView mRecyclerView;
 
     private void checkStatusBar(){
         Rect rectangle = new Rect();
@@ -40,12 +51,81 @@ public class LifeActivity extends MyBaseActivity {
         sb.edit().putInt("height",titleBarHeight).apply();
     }
 
+    private class MyHolder extends RecyclerView.ViewHolder{
+        public MyHolder(View itemView) {
+            super(itemView);
+        }
+
+        private TextView mText;
+
+        public TextView getText() {
+            return mText;
+        }
+
+        public void setText(TextView text) {
+            mText = text;
+        }
+    }
+    private class MyAdapter extends RecyclerView.Adapter<MyHolder>{
+        List<ResolveInfo> pkgAppsList = new ArrayList<>();
+        public MyAdapter() {
+            Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+            mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+            pkgAppsList = getPackageManager().queryIntentActivities( mainIntent, 0);
+        }
+
+        @Override
+        public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(THIS).inflate(R.layout.item_text,parent,false);
+            TextView textView = (TextView)view.findViewById(R.id.sssseeee);
+            MyHolder holder = new MyHolder(view);
+            holder.setText(textView);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(final MyHolder holder, int position) {
+            if(position<0){
+                return;
+            }
+            TextView tv = holder.getText();
+            ResolveInfo info = pkgAppsList.get(position);
+            tv.setText(info.activityInfo==null?"":info.activityInfo.packageName);
+            tv.setOnClickListener(new OnIntentClickListener() {
+                @Override
+                public void onClickWithGlobalCD(View v) {
+                    ResolveInfo info = pkgAppsList.get(holder.getAdapterPosition());
+                    String packageName = info.activityInfo==null?"":info.activityInfo.packageName;
+                    if(TextUtils.isEmpty(packageName)){
+                        return;
+                    }
+                    Intent launchIntent = getPackageManager().getLaunchIntentForPackage(packageName);
+                    startActivity( launchIntent );
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return pkgAppsList.size();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_test);
+        mCenterBtn = $View(R.id.missyou);
+        mRecyclerView = $View(R.id.packagerecy);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        mCenterBtn.setOnClickListener(new OnIntentClickListener() {
+            @Override
+            public void onClickWithGlobalCD(View v) {
+                mRecyclerView.setAdapter(new MyAdapter());
+            }
+        });
         mBgViewGroup = $View(R.id.myground);
         mBgViewGroup.setOnClickListener(new View.OnClickListener() {
             @Override
